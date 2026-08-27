7eeedb90623c8c37dfd883bac1a6f29cb32607aa
Matheus Monteiro <matheus.stack.20@gmail.com>
2026-08-27 10:03:42 -0300

    refactor(service): use decimal.Decimal for price to avoid rounding errors

--- internal/service/model.go
+++ internal/service/model.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/shopspring/decimal"
 	"gorm.io/gorm"
 )
 
@@ -11,7 +12,7 @@ type Service struct {
 	ID          uuid.UUID `gorm:"type:uuid;primaryKey;default:gen_random_uuid()"`
 	Name        string    `gorm:"not null"`
 	Description string
-	Price       float64 `gorm:"not null"`
+	Price       decimal.Decimal `gorm:"type:numeric(10,2);not null"`
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
